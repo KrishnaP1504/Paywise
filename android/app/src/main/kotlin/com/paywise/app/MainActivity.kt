213c70879254655990b3af62406809f9aadabf68
KrishnaP1504 <KrishnaP1504@users.noreply.github.com>
2026-09-20 22:33:43 +0530

    chore: temporarily disable FLAG_SECURE to permit showcase screenshots

--- android/app/src/main/kotlin/com/paywise/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/paywise/app/MainActivity.kt
@@ -7,6 +7,7 @@ import io.flutter.embedding.android.FlutterFragmentActivity
 class MainActivity: FlutterFragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
         window.setFlags(
             WindowManager.LayoutParams.FLAG_SECURE,
             WindowManager.LayoutParams.FLAG_SECURE
